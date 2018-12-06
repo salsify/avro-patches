08e347a7126fb6e1fdb01ee2867aaa6ce72b9f9d
Kyle Phelps <kphelps@salsify.com>
2018-12-06 14:42:43 -0500

    Add tests

--- lib/avro-patches/optimized_serde.rb
+++ lib/avro-patches/optimized_serde.rb
@@ -5,11 +5,34 @@ Avro::IO::BinaryDecoder.class_eval do
   end
 
   def read_float
-    @reader.read(4).unpack1('e'.freeze)
+    read_and_unpack(4, 'e'.freeze)
   end
 
   def read_double
-    @reader.read(8).unpack1('E'.freeze)
+    read_and_unpack(8, 'E'.freeze)
+  end
+
+  def read_string
+    read_bytes.tap do |string|
+      string.force_encoding('utf-8'.freeze) if string.respond_to?(:force_encoding)
+    end
+  end
+
+  private
+
+  # Optimize unpacking strings when `unpack1` is available (ruby >= 2.4)
+  if String.instance_methods.include?(:unpack1)
+
+    def read_and_unpack(byte_count, format)
+      @reader.read(byte_count).unpack1(format)
+    end
+
+  else
+
+    def read_and_unpack(byte_count, format)
+      @reader.read(byte_count).unpack(format)[0]
+    end
+
   end
 end
 
@@ -21,5 +44,10 @@ Avro::IO::BinaryEncoder.class_eval do
 
   def write_double(datum)
     @writer.write([datum].pack('E'.freeze))
+  end
+
+  def write_string(datum)
+    datum = datum.encode('utf-8'.freeze) if datum.respond_to?(:encode)
+    write_bytes(datum)
   end
 end
