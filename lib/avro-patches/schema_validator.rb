2338be0af1c2b3e9f03e5a25f3f66f8872273e7a
Tim Perkins <tim.perkins@ezcater.com>
2019-05-21 14:53:45 -0400

    Remove 1.8.2 patches

--- lib/avro-patches/schema_validator.rb
+++
@@ -1,5 +0,0 @@
-# Changes from "AVRO-1886: Add validation messages"
-# https://github.com/apache/avro/pull/111
-require_relative 'schema_validator/schema_validator'
-require_relative 'schema_validator/schema'
-require_relative 'schema_validator/io'
